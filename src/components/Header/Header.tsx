a8db32438e1934b16383bf1cbf80bdcaf541094c
unknown <pablo.mieres5@gmail.com>
2023-03-24 01:45:10 -0600

    Commit inicial

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className="logo">LOGO</div>
+
+        <div className="search-bar">SEARCH BAR</div>
+        <div className="header-links"> LINKS</div>
+      </div>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Head from "next/head";
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Brokery</title>
+        <meta name="description" content="Brokery App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Page */}
+      <Header></Header>
+      <main style={{ minHeight: "90vh" }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer></Footer>
+    </>
+  );
 }
